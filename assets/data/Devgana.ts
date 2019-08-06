3d14e6aae71aa6a378dcd47972e6aed225b8074e
devgana <52679891+devgana@users.noreply.github.com>
2019-08-06 17:35:23 -0400

    Add files via upload

---
+++ assets/data/Devgana.ts
@@ -0,0 +1,5 @@
+import{devgana} from '../../app/devgana';
+
+export const DEVGANA: devgana[] = [ 
+    {id: '991491123', name: 'Abhinandan devgan', login_name : 'devgana', campus:'davis', assignment_title:'devgana-A4'}];
+   
